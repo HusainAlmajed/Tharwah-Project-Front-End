b3ecf266aa35843636184c25e5892e117442ea43
Jassiiimm <owainatijassim@gmail.com>
2026-08-11 23:26:39 +0300

    Added the category list page

---
+++ src/pages/CategoryList.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react"
+import * as categoryService from "../services/category"
+import { Link } from "react-router"
+
+const CategoryList = () => {
+    return (
+    <div className="category-list">
+        <h1>Categories</h1>
+        </div>
+        )
+    }
+
+export default CategoryList
