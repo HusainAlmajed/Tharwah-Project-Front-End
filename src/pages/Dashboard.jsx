64584f5bf0ec61c8d981cab49b0bd76c615a58af
Husain <hussainalmajed.2005@gmail.com>
2026-08-11 22:43:27 +0300

    The total balance and expense can be calculated and displayed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,42 @@
 import { useEffect, useState } from "react"
 import { index } from '../services/user'
 import { useNavigate } from "react-router"
+import * as transactionService from "../services/transaction"
 
 const Dashboard = (props) => {
 
 const navigate = useNavigate()
 
-    const [allUsers, setAllUsers] = useState([])
+const[transactions , setTransactions] = useState([])
+const [totalIncome , setTotalIncome] = useState(0)
+const [totalExpenses , setTotalExpenses] = useState(0)
+const [balance , setBalance] = useState(0)
 
     useEffect(() => {
-        const fetchUsers = async () => {
-            const usersData =  await index()
-            setAllUsers(usersData)
+        const fetchTransaction = async () => {
+            const data =  await transactionService.index()
+            setTransactions(data)
+            console.log("data" , data)
+            // console.log("data" , data)
+            // setTotal(data.amount)
+
+            let income = 0
+            let expense = 0
+
+            data.map((transaction) => {
+                if (transaction.transactionType === 'Income') {
+                    income = income + transaction.amount
+                }else if (transaction.transactionType === 'Expense') {
+                    expense = expense + transaction.amount
+                }
+            })
+        setTotalIncome(income)
+        setTotalExpenses(expense)
+        setBalance(income - expense)
+        console.log(income)
+        console.log(totalIncome)
         }
-        fetchUsers()
+        fetchTransaction()
     }, [])
 
     return (
@@ -22,22 +45,21 @@ const navigate = useNavigate()
         <section className="cardSec">
             Income
                 <div className="card">
-                        <p>500$</p>
+                        <p>${totalIncome}</p>
                 </div>
             Expenses
                 <div className="card">
-                        <p>300$</p>
+                        <p>${totalExpenses}</p>
                 </div>
             Balance
                 <div className="card">
-                        <p>200$</p>
+                        <p>${balance}</p>
                 </div>
         </section>
 
         <section>
             Monthly Cash Flow
             <div className="monthlyCard">
-
             </div>
         </section>
         </div>
