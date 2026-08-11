de3d89145f3033531832316dbecada3c666c3aa8
Jassiiimm <owainatijassim@gmail.com>
2026-08-11 23:29:33 +0300

    Show all categories

--- src/pages/CategoryList.jsx
+++ src/pages/CategoryList.jsx
@@ -1,13 +1,33 @@
-import { useState, useEffect } from "react"
+import { useState , useEffect } from "react"
 import * as categoryService from "../services/category"
 import { Link } from "react-router"
 
 const CategoryList = () => {
+
+const [categories , setCategories] = useState([])
+const [loading , setLoading] = useState(true)
+
+useEffect(() => {
+    const fetchData = async () => {
+        const data = await categoryService.index()
+        setCategories(data)
+        setLoading(false)
+    }
+    fetchData()
+}, [])
+
+if (loading) return <main><div className="loader"></div></main>
+
     return (
     <div className="category-list">
         <h1>Categories</h1>
-        </div>
-        )
-    }
-
+        {categories.map((category) => (
+            <div className="category" key={category._id}>
+                <h3>{category.name}</h3>
+                </div>
+            ))}
+            </div>
+            )
+        }
+        
 export default CategoryList
