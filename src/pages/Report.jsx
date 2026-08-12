1d177c5e07d2ba4ea33172fca71f846208858c42
Jassiiimm <owainatijassim@gmail.com>
2026-08-12 10:24:21 +0300

    Added monthly and yearly buttons in report

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react"
+
 const Report = () => {
+    const [reportType, setReportType] = useState("Monthly")
+
     return (
-    <div>
-        <h1>Reports</h1>
-        <p>Test</p>
+        <div>
+            <h1>Reports</h1>
+            <p>Test</p>
+            
+            <div>
+                <label>View</label>
+                <button
+                    type="button"
+                    onClick={() => setReportType("Monthly")}
+                >
+                    Monthly
+                </button>
+
+                <button
+                    type="button"
+                    onClick={() => setReportType("Yearly")}
+                >
+                    Yearly
+                    </button>
+            </div>
         </div>
     )
 }
